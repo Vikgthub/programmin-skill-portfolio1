c31e996d017247e12c7ff774dc0f5b7e67edba4d
Vikgthub <164849216+Vikgthub@users.noreply.github.com>
2024-10-24 21:17:34 +0400

    Update Ex4 with bonus.py

--- Ex4 with bonus .py
+++ Ex4 with bonus .py
@@ -18,7 +18,7 @@ a1 = one.capitalize()
 if a1 == France:
     print ("correct answer, greatjob!")
 else:
-    print ("wrong anwer!!! Correct ans = Paris")
+    print ("wrong answer!!! Correct ans = Paris")
     
 # Same applies to other variables and statements
 two = str(input("Enter the capital of Germany: "))
@@ -26,63 +26,63 @@ b1 = two.capitalize()
 if b1 == Germany:
     print ("correct answer, greatjob!")
 else:
-    print ("wrong anwer!!! Correct ans = Berlin")
+    print ("wrong answer!!! Correct ans = Berlin")
 
 three = str(input("Enter the capital of Italy: "))
 c1 = three.capitalize()
 if c1 == Italy:
     print ("correct answer, greatjob!")
 else:
-    print ("wrong anwer!!! Correct ans = Rome")
+    print ("wrong answer!!! Correct ans = Rome")
 
 four = str(input("Enter the capital of Spain: "))
 d1 = four.capitalize()
 if d1 == Spain:
     print ("correct answer, greatjob!")
 else:
-    print ("wrong anwer!!! Correct ans = Madrid")
+    print ("wrong answer!!! Correct ans = Madrid")
 
 five = str(input("Enter the capital of Norway: "))
 e1 = five.capitalize()
 if e1 == Norway:
     print ("correct answer, greatjob!")
 else:
-    print ("wrong anwer!!! Correct ans = Oslo" )
+    print ("wrong answer!!! Correct ans = Oslo" )
 
 six = str(input("Enter the capital of Netherlands: "))
 f1 = six.capitalize()
 if f1 == Netherlands:
     print ("correct answer, greatjob!")
 else:
-    print ("wrong anwer!!! Correct ans = Amsterdam")
+    print ("wrong answer!!! Correct ans = Amsterdam")
 
 seven = str(input("Enter the capital of Austria: "))
 g1 = seven.capitalize()
 if g1 == Austria:
     print ("correct answer, greatjob!")
 else:
-    print ("wrong anwer!!! Correct ans = Vienna")
+    print ("wrong answer!!! Correct ans = Vienna")
 
 eight = str(input("Enter the capital of Romania: "))
 h1 = eight.capitalize()
 if h1 == Romania:
     print ("correct answer, greatjob!")
 else:
-    print ("wrong anwer!!! Correct ans = Bucharest")
+    print ("wrong answer!!! Correct ans = Bucharest")
 
 nine = str(input("Enter the capital of Iceland: "))
 i1 = nine.capitalize()
 if i1 == Iceland:
     print ("correct answer, greatjob!")
 else:
-    print ("wrong anwer!!! Correct ans = Reykjavik")
+    print ("wrong answer!!! Correct ans = Reykjavik")
     
 ten = str(input("Enter the capital of Slovenia: "))
 j1 = ten.capitalize()
 if j1 == Slovenia:
     print ("correct answer, greatjob!")
 else:
-    print ("wrong anwer!!! Correct ans = Ljubljana")
+    print ("wrong answer!!! Correct ans = Ljubljana")
   
  # At the end once the quiz has finished, the programm will print completed quiz message 
 print("Congrats, Quiz Completed!")
