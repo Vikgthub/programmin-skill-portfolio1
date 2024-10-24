334f7abd7ed8e1d79ddec64dd31160b419f356f9
Vikgthub <164849216+Vikgthub@users.noreply.github.com>
2024-10-24 21:15:49 +0400

    Ex10.py

--- Ex10.py
+++ Ex10.py
@@ -1,7 +1,7 @@
 # create variable to get number from user 
 numo = int(input("Enter number to check if odd or even: "))
 
-# create variable is divisible by 2 and remainder is 0 then program prints message thay number is even 
+# if variable is divisible by 2 and remainder is 0 then program prints message thay number is even 
 if numo % 2 == 0:
     print("The Number You Just Entered Is _Even!")
     # otherwise program prints its odd 
